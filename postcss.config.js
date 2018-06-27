7015e0071faff61d05a57ffa7a2bf9bc3a0e1fb3
cnyet <yateup@gmail.com>
2018-06-27 09:52:01 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {},
+    //压缩和优化样式表的功能
+    'cssnano': {},
+    //处理包含类似sass的新特性
+    "precss": {},
+  }
+}
